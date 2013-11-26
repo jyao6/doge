50d45a12d7741e0a58cec54186cd84623f2492a9
Jessica Yao <jyao@Jessicas-MacBook-Pro-2.local>
2013-11-25 21:06:26 -0500

    rise of the dogestar

---
+++ dogestar/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dogestar::Application.config.secret_key_base = 'f7a313a24a5eb79944a2b89c91d8708c2bb638d216375198827a58aa7b33f27a270817930eefea1c95460f2b047df99b0bff1e40e28114fbff3aa366c0d2397d'
