c89032f1801c50c9eb923ed9af3e2a7b6de344f7
ivamilo <ivamilo@college.harvard.edu>
2014-01-05 01:44:31 -0500

    change email index to be unique. add basic user model and tests

---
+++ dogestar/db/migrate/20140105063207_add_unique_email_to_users.rb
@@ -0,0 +1,6 @@
+class AddUniqueEmailToUsers < ActiveRecord::Migration
+  def change
+    remove_index :users, :email
+    add_index :users, :email, unique: true
+  end
+end
--- dogestar/db/schema.rb
+++ dogestar/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140105055725) do
+ActiveRecord::Schema.define(version: 20140105063207) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -24,6 +24,6 @@ ActiveRecord::Schema.define(version: 20140105055725) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
