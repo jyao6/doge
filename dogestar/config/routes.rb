8f3164098523044caba195dab40abbaeab32968b
ivamilo <ivamilo@college.harvard.edu>
2014-01-07 18:45:50 -0500

    user signup file blank so far

--- dogestar/config/routes.rb
+++ dogestar/config/routes.rb
@@ -1,9 +1,13 @@
 Dogestar::Application.routes.draw do
+  get "users/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+
+  resources :users
+  match '/signup', to: 'users#new', via: 'get'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
