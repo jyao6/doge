a8fa30549b0838de6abf967e8f50910c215d1355
ivamilo <ivamilo@college.harvard.edu>
2014-01-12 00:36:35 -0500

    enum for categories

--- dogestar/app/models/service.rb
+++ dogestar/app/models/service.rb
@@ -1,6 +1,21 @@
 class Service < ActiveRecord::Base
-	belongs_to :user
-	has_many :photos, dependent: :destroy
-	has_many :reviews, dependent: :destroy
-	has_many :transactions
+  belongs_to :user
+  has_many :photos, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :transactions
+
+  CATS = { "Category 1"=>1, "Category 2"=>2, "Category 3"=>3}
+
+  def self.categories
+    CATS.keys
+  end
+
+  def category
+    CATS.key(read_attribute(:category))
+  end
+
+  def category=(n)
+    write_attribute(:category, CATS[n])
+  end
+
 end
