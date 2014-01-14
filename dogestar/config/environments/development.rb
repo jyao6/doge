992e809de277f3a6e3dce28f3d871e4b6239be9a
ivamilo <ivamilo@college.harvard.edu>
2014-01-14 16:52:49 -0500

    paperclip gem

--- dogestar/config/environments/development.rb
+++ dogestar/config/environments/development.rb
@@ -26,4 +26,7 @@ Dogestar::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # ImageMagick path. Obtained from which convert
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
