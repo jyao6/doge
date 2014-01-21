907ec2196005b3e3dca34ad62a1dfc4051181788
ivamilo <ivamilo@college.harvard.edu>
2014-01-21 03:31:55 -0500

    add reset password table to store tokens

--- dogestar/db/schema.rb
+++ dogestar/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140118052929) do
+ActiveRecord::Schema.define(version: 20140121052230) do
 
   create_table "messages", force: true do |t|
     t.text     "body"
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 20140118052929) do
   end
 
   add_index "photos", ["service_id"], name: "index_photos_on_service_id", using: :btree
+
+  create_table "reset_passwords", force: true do |t|
+    t.integer  "user_id"
+    t.string   "token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "reviews", force: true do |t|
     t.integer  "rating"
