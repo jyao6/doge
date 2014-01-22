b11af7f10c5256662b503b671ea3000a49535359
ivamilo <ivamilo@college.harvard.edu>
2014-01-22 16:55:27 -0500

    utc to current time converter function

--- dogestar/app/helpers/community_helper.rb
+++ dogestar/app/helpers/community_helper.rb
@@ -6,8 +6,16 @@ module CommunityHelper
     "Harvard University"
   end
 
+  def current_tz_region
+  	COMMUNITIES[current_community][:timezone]
+  end
+
   def current_timezone
-    Time.find_zone(COMMUNITIES[current_community][:timezone])
+    Time.find_zone(current_tz_region)
+  end
+
+  def utc2current(utc_time)
+    utc_time.in_time_zone(current_tz_region)
   end
 
 end
